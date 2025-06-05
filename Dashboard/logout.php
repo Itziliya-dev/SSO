4cd1791eb94a712f949998f208247ff83ea18144
Itz_iliya32 <iliyabejanle@gmail.com>
2025-06-06 01:46:41 +0330

    New location

---
+++ Dashboard/logout.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+// پاک کردن تمام متغیرهای session
+$_SESSION = array();
+
+// پاک کردن کوکی session
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// نابود کردن session
+session_destroy();
+
+// پاک کردن کوکی sso_token
+setcookie('sso_token', '', time() - 3600, '/', '.itziliya-dev.ir', true, true); // دامنه را بررسی کنید
+
+// هدایت به صفحه ورود
+header('Location: /../login.php');
+exit();
+?>
