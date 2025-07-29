a3d7c6a270a2d35152ca570199a4d7a56e827fce
Itz_iliya32 <iliyabejanle@gmail.com>
2025-07-30 02:23:06 +0330

    Add a shared header

--- includes/header.php
+++ includes/header.php
@@ -10,6 +10,16 @@ require_once __DIR__ . '/config.php';
     
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+
+    <link rel="stylesheet" href="/assets/css/admin.css">
+    <link rel="stylesheet" href="/assets/css/admin_dashboard_redesign.css">
+    <link rel="stylesheet" href="/assets/fonts/Vazirmatn-font-face.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+
     
 </head>
-<body>
+<?php
+$current_theme = $settings['vui_theme'] ?? 'vui-theme-default'; 
+?>
+<body class="<?= htmlspecialchars($current_theme) ?>">
